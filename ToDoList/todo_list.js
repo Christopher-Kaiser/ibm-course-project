ed90b7f7eb7da8ab5c145c8d25a1b4b9d732efb1
Christopher-Kaiser <christophermsindo97@gmail.com>
2025-05-20 06:54:46 -0400

    added a functionality to clear all tasks in the array

--- ToDoList/todo_list.js
+++ ToDoList/todo_list.js
@@ -35,5 +35,11 @@ function clearCompletedTasks() {
     displayTasks();
 }
 
+function clearAllTasks() {
+    tasks = [];
+    displayTasks()
+}
+
 addTaskBtn.addEventListener("click", addTask);
 clearCompletedBtn.addEventListener("click", clearCompletedTasks);
+clearAllButton.addEventListener("click", clearAllTasks);
